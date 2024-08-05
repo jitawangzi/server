a1bf031991dc7db13cb65eecc077ebd291a964a0
Pparty_shuyongqiang <287424271@qq.com>
2024-08-05 19:01:32 +0800

    json不序列化默认值，减少点空间占用

--- game/src/test/java/GG2.java
+++ game/src/test/java/GG2.java
@@ -1,14 +1,31 @@
-import java.util.HashMap;
+import java.util.Map;
+
+import cn.game.util.JsonUtil;
 
 public class GG2 {
 
 	public static void main(String[] args) throws Exception {
+		JSTest jsTest = new JSTest();
+		jsTest.i2 = 2;
+
+		String jsonString = JsonUtil.toJsonString(jsTest);
+		System.out.println(jsonString);
+		GG2.JSTest object = JsonUtil.parseObject(jsonString, JSTest.class);
+		System.out.println(object.i);
+		System.out.println(object.i2);
 		
+		Map<Integer, JSTest> map = new java.util.HashMap<>();
+		map.put(1, jsTest);
+
+		String mapString = JsonUtil.toJsonString(map);
+		System.err.println(mapString);
 
 	}
 
-	static class MapObj {
-		public HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+	static class JSTest {
+		public String str;
+		public int i;
+		public int i2;
 	}
 
 }
