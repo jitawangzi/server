200747eac601bc92f81671cb5de7ae655e1527e0
Pparty_shuyongqiang <287424271@qq.com>
2024-09-04 10:23:14 +0800

    去掉重复定义包

--- simulationclient/src/main/java/cn/game/simulation/test/gen/MailSeeRequest_12000003Test.java
+++ simulationclient/src/main/java/cn/game/simulation/test/gen/MailSeeRequest_12000003Test.java
@@ -1,8 +1,7 @@
 package cn.game.simulation.test.gen;
 
-import org.springframework.stereotype.Component;
-
 import com.google.protobuf.Message;
+import org.springframework.stereotype.Component;
 
 import cn.game.simulation.client.Client;
 import cn.game.simulation.client.ServerTestContext;
@@ -14,6 +13,7 @@ public class MailSeeRequest_12000003Test extends ServerTest{
 	@Override
 	public Message getMessage(Client client) {
 		cn.game.protocol.protobuf.MailMsg.MailSeeRequest_12000003.Builder builder = cn.game.protocol.protobuf.MailMsg.MailSeeRequest_12000003.newBuilder() ; 
+		
 		
 		
 		return builder.build() ; 
