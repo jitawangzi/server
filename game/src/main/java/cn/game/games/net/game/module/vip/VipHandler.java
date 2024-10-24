716e627a17f422099ba0fe9f831f6affd41c7a51
liuyang_cp <mengqing20101212@163.com>
2024-10-24 10:30:09 +0800

    VIP 免费礼包每天都能领取

--- game/src/main/java/cn/game/games/net/game/module/vip/VipHandler.java
+++ game/src/main/java/cn/game/games/net/game/module/vip/VipHandler.java
@@ -55,10 +55,10 @@ public class VipHandler extends BaseHandler {
             return;
         }
         VIPConfig vipConfig = vipModule.getCurVipConfig();
-        if (vipModule.getRewardFreeGiftList().contains(vipConfig.ID)){
+       /* if (vipModule.getRewardFreeGiftList().contains(vipConfig.ID)){
             client.sendProtocol(res, ErrorMsgEnum.vip_free_gift_has_reward.getId());
             return;
-        }
+        }*/
         vipModule.setRewardFreeGiftTimer(now);
         vipModule.getRewardFreeGiftList().add(vipConfig.ID);
         res.addAllDrops(PlayerHelper.addReward(player,vipConfig.DailyBox,OpType.vipGiftReward));
