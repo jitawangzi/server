fd0162a5c7f60835aedde672a331c07998e7e528
Pparty_shuyongqiang <287424271@qq.com>
2024-12-19 14:48:05 +0800

    老号活动开启

--- game/src/main/java/cn/game/games/net/game/module/activity/ActivityModule.java
+++ game/src/main/java/cn/game/games/net/game/module/activity/ActivityModule.java
@@ -31,7 +31,8 @@ import cn.game.protocol.protobuf.RewardMsg.RewardInfo;
 import cn.game.util.DateUtil;
 
 public class ActivityModule extends BasePlayerModule {
-	private static EventTypeEnum[] events = new EventTypeEnum[] { EventTypeEnum.PLAYER_CREATE, EventTypeEnum.NewDay, EventTypeEnum.NewWeek,
+	private static EventTypeEnum[] events = new EventTypeEnum[] { EventTypeEnum.PLAYER_CREATE, EventTypeEnum.LoginFinish,
+			EventTypeEnum.NewDay, EventTypeEnum.NewWeek,
 			EventTypeEnum.NewMonth, EventTypeEnum.LevelUp };
 	/** 已经开始的活动，只是展示的不在这里。  */
 	private Map<Integer, ActivityBase> activities = new HashMap<Integer, ActivityBase>();
@@ -80,7 +81,7 @@ public class ActivityModule extends BasePlayerModule {
 
 	private void initNonTimeNewDayActivity() {
 
-		List<ActivityConfig> openTypeList = ActivityManager.instance().getOpenTypeList(ActivityHelper.OPENTYPE_PLAYER_CREATE_DAYS);
+		Collection<ActivityConfig> openTypeList = ActivityManager.instance().list();
 		if (openTypeList != null){
 			for (ActivityConfig activityConfig : openTypeList) {
 				if (canOpenNonTimeOpeningActivity(activityConfig)) {
@@ -365,6 +366,10 @@ public class ActivityModule extends BasePlayerModule {
 			refreshByType(1);
 			newDay();
 			checkResetCycleActivity();
+			initNonTimeNewDayActivity();
+			break;
+		}
+		case LoginFinish: {
 			initNonTimeNewDayActivity();
 			break;
 		}
