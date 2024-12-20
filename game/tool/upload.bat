39843652c5804a07223892565b2962ca48b84fdc
Pparty_shuyongqiang <287424271@qq.com>
2024-12-20 17:54:54 +0800

    junit调整

--- game/tool/upload.bat
+++ game/tool/upload.bat
@@ -3,7 +3,7 @@ pscp -pw root %workspace%\game/target/game.zip root@test:/server/game
 
 @echo stop
 plink -pw root root@test source /etc/profile; cd /server/game/gameserver; /server/game/gameserver/game.sh stop
-ping -n 5 127.1>nul
+::ping -n 5 127.1>nul
 
 @echo install 
 plink -pw root root@test source /etc/profile; /server/bin/install_game.sh; 
