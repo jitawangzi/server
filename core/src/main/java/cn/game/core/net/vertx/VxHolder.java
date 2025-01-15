9d69cb3302a99c5afa991b52c42765a34e1f6252
Pparty_shuyongqiang <287424271@qq.com>
2025-01-15 17:58:21 +0800

    1

--- core/src/main/java/cn/game/core/net/vertx/VxHolder.java
+++ core/src/main/java/cn/game/core/net/vertx/VxHolder.java
@@ -454,7 +454,7 @@ public class VxHolder {
 				T result = blockingCode.execute();
 				promise.complete(result);
 			} catch (Throwable e) {
-				log.error("error executeBlockingWithTimeout", e);
+				log.error("executeBlockingWithTimeout error", e);
 				promise.fail(e);
 			}
 		}, timeoutMs, ordered);
