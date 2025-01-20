783882a4ed2bec4da622349127e91345bc023905
Pparty_shuyongqiang <287424271@qq.com>
2025-01-20 15:50:27 +0800

    1

--- core/src/main/java/cn/game/core/util/AsyncUtils.java
+++ core/src/main/java/cn/game/core/util/AsyncUtils.java
@@ -3,8 +3,26 @@ package cn.game.core.util;
 import java.util.concurrent.TimeUnit;
 
 import io.vertx.core.Future;
+import io.vertx.core.impl.VertxThread;
 
+/**    
+ * 异步转同步工具类
+ * 注意不要在vert.x的EventLoop线程中阻塞等待结果，会让线程无法处理其他事件， 
+ * 也包括本Future可能的事件。出现“卡死”现象
+ * 
+ * 2025年1月20日 15:40:19
+ * @author SYQ
+ */
 public class AsyncUtils {
+
+	public static void checkEventLoop() {
+		Thread currentThread = Thread.currentThread();
+		if (currentThread instanceof VertxThread && ((VertxThread) currentThread).isWorker() == false) {
+			throw new IllegalStateException(
+					"Blocking operation cannot be executed on EventLoop thread. " + "Current thread: " + currentThread.getName());
+		}
+	}
+
 	/** 
 	 * 同步等待Vertx 的Future完成
 	 * @param <T>
@@ -14,7 +32,7 @@ public class AsyncUtils {
 	 * @return
 	 */
 	public static <T> T await(Future<T> future, long timeout, TimeUnit unit) {
-		// 超时机制
+		checkEventLoop();
 		try {
 			return future.toCompletionStage().toCompletableFuture().get(timeout, unit);
 		} catch (Exception e) {
@@ -29,10 +47,12 @@ public class AsyncUtils {
 	 * @return
 	 */
 	public static <T> T await(Future<T> future) {
+		checkEventLoop();
 		return await(future, 30, TimeUnit.SECONDS); // 默认超时 30 秒
 	}
 
 	public static <T> T join(Future<T> future) {
+		checkEventLoop();
 		return future.toCompletionStage().toCompletableFuture().join();
 	}
 }
