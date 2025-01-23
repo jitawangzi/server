ab6707af577a8075d3eed561e7cd4b6dc21c66bf
liuyang_cp <mengqing20101212@163.com>
2025-01-23 14:25:09 +0800

    检测签到活动是否结束

--- game/src/main/java/cn/game/games/net/game/module/activity/impl/player/SevenDaysSignin.java
+++ game/src/main/java/cn/game/games/net/game/module/activity/impl/player/SevenDaysSignin.java
@@ -3,6 +3,7 @@ package cn.game.games.net.game.module.activity.impl.player;
 import java.util.ArrayList;
 import java.util.List;
 
+import cn.game.games.core.event.GameEvent;
 import cn.game.games.net.game.helper.MailHelper;
 import cn.game.games.net.game.module.award.Goods;
 import cn.game.protocol.generated.config.ActivityConfig;
@@ -27,7 +28,7 @@ import cn.game.protocol.protobuf.RewardMsg.RewardInfo;
 
 @ActivityType(type = ActivityTypeEnum.SevenDaysSignin)
 public class SevenDaysSignin extends PlayerActivityBase {
-	private static transient EventTypeEnum[] events = new EventTypeEnum[] {};
+	private static transient EventTypeEnum[] events = new EventTypeEnum[] {EventTypeEnum.LoginSuccess };
 	/** 今天登陆是否签到了 */
 	private boolean isSignin;
 	/** 已经签到的天数   1 - 7 */
@@ -44,6 +45,20 @@ public class SevenDaysSignin extends PlayerActivityBase {
 		builder.setCanSignin(!isSignin);
 		builder.addAllExtRewardIds(rewardExtra);
 		return builder.build();
+	}
+
+	@Override
+	public void handleEvent(GameEvent event) {
+		switch (event.getType()){
+			case LoginSuccess -> checkIsOver();
+		}
+	}
+
+	private void checkIsOver() {
+		ActivityConfig activityConfig = ActivityManager.instance().get(getId());
+		if (day == getSevenDaysSigninConfigList(id).size() && activityConfig.resetType != 4) {//7日签到结束就销毁
+			player.getActivityModule().destroy(super.id, true);
+		}
 	}
 
 	@Override
@@ -70,9 +85,8 @@ public class SevenDaysSignin extends PlayerActivityBase {
 	}
 
 	@Override
-	public List<RewardInfo> receive(int id) {
+	public List<RewardInfo> receive(int taskId) {
 		SevenDaysSigninConfig config = getSevenDaysSigninConfig(day);
-		ActivityConfig activityConfig = ActivityManager.instance().get(getId());
 		int addRadio = 0;
 		if (getSevenDaysSigninConfigList(this.getId()).size() > 7 ){//月卡才有加成，7日的 没有加成
 			addRadio = player.getWelfareValue(WelfareTypeEnum.MonthClock);
@@ -83,9 +97,7 @@ public class SevenDaysSignin extends PlayerActivityBase {
 		day++;
 		isSignin = true;
 		GameLogger.activity(player, super.id, day);
-		if (day == getSevenDaysSigninConfigList(id).size() && activityConfig.resetType != 4) {//7日签到结束就销毁
-			player.getActivityModule().destroy(super.id, true);
-		}
+		checkIsOver();
 		return resources;
 	}
 
