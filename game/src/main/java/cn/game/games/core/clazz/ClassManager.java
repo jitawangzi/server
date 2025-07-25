86ded068dd93c962bd343313a025d544a932aae4
shuyongqiang <shuyongqiang@cyou-inc.com>
2025-07-25 11:19:29 +0800

    优化计数

--- game/src/main/java/cn/game/games/core/clazz/ClassManager.java
+++ game/src/main/java/cn/game/games/core/clazz/ClassManager.java
@@ -19,117 +19,141 @@ import cn.game.util.file.PackageScanner;
 import cn.game.util.reflect.ClassHelper;
 
 public class ClassManager {
-	private static ClassManager instance = new ClassManager() ; 
-	Map<Integer, Class<? extends AbstractCondition>> conditionClass = new HashMap<Integer, Class<? extends AbstractCondition>>();
-	Map<Integer, Class<? extends ActivityBase>> activityClass = new HashMap<Integer, Class<? extends ActivityBase>>();
+    private static ClassManager instance = new ClassManager();
+    private final Map<Integer, Class<? extends AbstractCondition>> conditionClass = new HashMap<>();
+    private final Map<Integer, Class<? extends ActivityBase>> activityClass = new HashMap<>();
 
-	private ClassManager() {
-	};
-	
-	public static ClassManager getInstance() {
-		return instance;
-	}
+    // 缓存所有Condition实例，饿汉式加载
+    private final Map<Integer, AbstractCondition> conditionClassInstanceCache = new HashMap<>();
 
-	public void init() {
-		initConditionClass();
-		initActivityClass();
+    private ClassManager() {
+    }
 
-	}
+    public static ClassManager getInstance() {
+        return instance;
+    }
 
-	/** 
-	 * 初始化条件类class
-	 */
-	private void initConditionClass() {
+    public void init() {
+        initConditionClass();
+        initActivityClass();
+    }
 
-		Set<Class<? extends AbstractCondition>> subclasses = ClassHelper.findSubclasses(AbstractCondition.class.getPackageName(), AbstractCondition.class);
-		for (Class<? extends AbstractCondition> c : subclasses) {
-			ConditionType annotation = c.getAnnotation(ConditionType.class); 
-			if (annotation == null) {
-				throw new IllegalArgumentException(c.getName() + " 没有配置ConditionType注解");
-			}
-			ConditionTypeEnum type = annotation.type();
-			if (conditionClass.containsKey(type.ID)) {
-				throw new IllegalArgumentException(
-						MessageFormat.format("class[{0}] 配置了重复的ConditionType注解 [{1}]，已经在[{2}]里配置了", c.getName(), type,
-								conditionClass.get(type.ID)));
-			}
-			conditionClass.put(type.ID, c);
-		}
-	}
+    /** 
+     * 初始化条件类class和实例缓存（饿汉式加载实例）
+     */
+    private void initConditionClass() {
+        conditionClass.clear();
+        conditionClassInstanceCache.clear();
 
-	public Class<? extends AbstractCondition> getConditionClass(int type) {
-		return conditionClass.get(type);
-	}
+        Set<Class<? extends AbstractCondition>> subclasses = ClassHelper.findSubclasses(AbstractCondition.class.getPackageName(), AbstractCondition.class);
+        for (Class<? extends AbstractCondition> c : subclasses) {
+            ConditionType annotation = c.getAnnotation(ConditionType.class);
+            if (annotation == null) {
+                throw new IllegalArgumentException(c.getName() + " 没有配置ConditionType注解");
+            }
+            ConditionTypeEnum type = annotation.type();
+            if (conditionClass.containsKey(type.ID)) {
+                throw new IllegalArgumentException(
+                    MessageFormat.format("class[{0}] 配置了重复的ConditionType注解 [{1}]，已经在[{2}]里配置了", c.getName(), type,
+                        conditionClass.get(type.ID)));
+            }
+            conditionClass.put(type.ID, c);
 
-	public AbstractCondition createConditionClassInstance(int type) {
-		AbstractCondition newInstance = null;
-		try {
-			Class<? extends AbstractCondition> clazz = conditionClass.get(type);
-			if (clazz == null) {
-				throw new IllegalArgumentException("ConditionClass is null, condition type : " + type);
-			}
-			newInstance = clazz.getDeclaredConstructor().newInstance();
-		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
-				| NoSuchMethodException | SecurityException e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		}
-		return newInstance;
-	}
+            // 直接创建实例放到缓存
+            try {
+                AbstractCondition instance = c.getDeclaredConstructor().newInstance();
+                conditionClassInstanceCache.put(type.ID, instance);
+            } catch (Exception e) {
+                throw new RuntimeException("创建Condition实例失败: " + c.getName(), e);
+            }
+        }
+    }
 
-	/** 
-	 * 初始化活动类class
-	 */
-	private void initActivityClass() {
+    public Class<? extends AbstractCondition> getConditionClass(int type) {
+        return conditionClass.get(type);
+    }
 
-		Set<Class<? extends ActivityBase>> subclasses = ClassHelper
-				.findSubclasses(ActivityBase.class.getPackageName(), ActivityBase.class);
-		for (Class<? extends ActivityBase> c : subclasses) {
-			ActivityType annotation = c.getAnnotation(ActivityType.class);
-			if (annotation == null) {
-				throw new IllegalArgumentException(c.getName() + " 没有配置ActivityType注解");
-			}
-			ActivityTypeEnum type = annotation.type();
-			if (activityClass.containsKey(type.ID)) {
-				throw new IllegalArgumentException(
-						MessageFormat.format("class[{0}] 配置了重复的ActivityType注解 [{1}]，已经在[{2}]里配置了", c.getName(), type,
-								activityClass.get(type.ID)));
-			}
-			activityClass.put(type.ID, c);
-		}
-	}
+    /** 
+     * 每次创建新的实例
+     * @param type
+     * @return
+     */
+    public AbstractCondition createConditionClassInstance(int type) {
+        AbstractCondition newInstance = null;
+        try {
+            Class<? extends AbstractCondition> clazz = conditionClass.get(type);
+            if (clazz == null) {
+                throw new IllegalArgumentException("ConditionClass is null, condition type : " + type);
+            }
+            newInstance = clazz.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+                | NoSuchMethodException | SecurityException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        return newInstance;
+    }
 
-	public Class<? extends ActivityBase> getActivityClass(int type) {
-		return activityClass.get(type);
-	}
+    /**
+     * 获取缓存的实例
+     */
+    public AbstractCondition getCachedConditionClassInstance(int type) {
+        return conditionClassInstanceCache.get(type);
+    }
 
-	public ActivityBase createActivityClassInstance(int type) {
-		ActivityBase newInstance = null;
-		try {
-			Class<? extends ActivityBase> clazz = activityClass.get(type);
-			if (clazz == null) {
-				throw new IllegalArgumentException("ActivityClass is null, activity type : " + type);
-			}
-			newInstance = clazz.getDeclaredConstructor().newInstance();
-		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
-				| NoSuchMethodException | SecurityException e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		}
-		return newInstance;
-	}
+    /** 
+     * 初始化活动类class
+     */
+    private void initActivityClass() {
+        activityClass.clear();
+        Set<Class<? extends ActivityBase>> subclasses = ClassHelper
+                .findSubclasses(ActivityBase.class.getPackageName(), ActivityBase.class);
+        for (Class<? extends ActivityBase> c : subclasses) {
+            ActivityType annotation = c.getAnnotation(ActivityType.class);
+            if (annotation == null) {
+                throw new IllegalArgumentException(c.getName() + " 没有配置ActivityType注解");
+            }
+            ActivityTypeEnum type = annotation.type();
+            if (activityClass.containsKey(type.ID)) {
+                throw new IllegalArgumentException(
+                        MessageFormat.format("class[{0}] 配置了重复的ActivityType注解 [{1}]，已经在[{2}]里配置了", c.getName(), type,
+                                activityClass.get(type.ID)));
+            }
+            activityClass.put(type.ID, c);
+        }
+    }
 
-	public List<Class<?>> scanClassesWithAnnotation(String packageName, Class<? extends Annotation> annotation) {
-		List<Class<?>> classes = new ArrayList<>();
-		try {
-			for (Class<?> clazz : PackageScanner.scanClasses(packageName)) {
-				if (clazz.isAnnotationPresent(annotation)) {
-					classes.add(clazz);
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return classes;
-	}
+    public Class<? extends ActivityBase> getActivityClass(int type) {
+        return activityClass.get(type);
+    }
+
+    public ActivityBase createActivityClassInstance(int type) {
+        ActivityBase newInstance = null;
+        try {
+            Class<? extends ActivityBase> clazz = activityClass.get(type);
+            if (clazz == null) {
+                throw new IllegalArgumentException("ActivityClass is null, activity type : " + type);
+            }
+            newInstance = clazz.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+                | NoSuchMethodException | SecurityException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        return newInstance;
+    }
+
+    public List<Class<?>> scanClassesWithAnnotation(String packageName, Class<? extends Annotation> annotation) {
+        List<Class<?>> classes = new ArrayList<>();
+        try {
+            for (Class<?> clazz : PackageScanner.scanClasses(packageName)) {
+                if (clazz.isAnnotationPresent(annotation)) {
+                    classes.add(clazz);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return classes;
+    }
 }
--- game/src/main/java/cn/game/games/net/game/module/player/CountingModule.java
+++ game/src/main/java/cn/game/games/net/game/module/player/CountingModule.java
@@ -1,36 +1,136 @@
 package cn.game.games.net.game.module.player;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import cn.game.games.core.BasePlayerModule;
+import cn.game.games.core.clazz.ClassManager;
 import cn.game.games.core.event.EventTypeEnum;
 import cn.game.games.core.event.PlayerEvent;
+import cn.game.games.net.game.module.quest.AbstractCondition;
+import cn.game.protocol.generated.config.ConditionConfig;
+import cn.game.protocol.generated.config.HeroConfig;
+import cn.game.protocol.generated.enume.Asset;
 import cn.game.protocol.generated.enume.ConditionTypeEnum;
-import cn.game.protocol.protobuf.PlayerMsg;
+import cn.game.protocol.generated.manager.ConditionManager;
+import cn.game.protocol.generated.manager.HeroManager;
 import cn.game.protocol.protobuf.PlayerMsg.PlayerAllInfo.Builder;
 import cn.game.util.IntMapWrapper;
 import cn.game.util.StringMapWrapper;
 
 /**    
  * 独立的计数模块，用来记录各种计数变量
- * 可以包含累计的计数，需要重置的计数等
+ * 这些计数的生命周期比较独立，是单独计算的，有自己固定的重置周期
+ * 例如可以一直累计，也可以按天刷新，按周刷新等。 
+ * 通常和条件表配合使用。 
  * 2025年7月24日 17:07:31
  * @author SYQ
  */
 public class CountingModule extends BasePlayerModule {
-	private static transient EventTypeEnum[] events = new EventTypeEnum[] { EventTypeEnum.PLAYER_CREATE };
-
-	/** 一些累计的计数， 类型->数量*/
-	private IntMapWrapper cumulativeCountMap = new IntMapWrapper();
-	/** 一些累计的计数,类型->数量 ，类型带额外参数的 */
-	private StringMapWrapper cumulativeCountExtMap = new StringMapWrapper();
+	private static final EventTypeEnum[] events = initEventTypes();
+	/** 刷新类型：  类型->数量*/
+	private Map<Integer, IntMapWrapper> cumulativeCountMap = new HashMap<Integer, IntMapWrapper>();
+	/** 刷新类型：  类型->数量，类型带额外参数的  */
+	private Map<Integer, StringMapWrapper> cumulativeCountExtMap = new HashMap<Integer, StringMapWrapper>();
 	
+    private static EventTypeEnum[] initEventTypes() {
+    	List<EventTypeEnum> eventTypeList = new java.util.ArrayList<>();
+    	for (ConditionTypeEnum type : ConditionTypeEnum.values()) {
+			if (type.countType == 2) {
+				AbstractCondition cachedConditionClassInstance = ClassManager.getInstance().getCachedConditionClassInstance(type.ID);
+				EventTypeEnum[] eventTypes = cachedConditionClassInstance.getEventTypes(); 
+				Collections.addAll(eventTypeList, eventTypes) ; 
+			}
+		}
+    	return eventTypeList.toArray(new EventTypeEnum[0]);
+    }
+    
 	@Override
 	public void handleEvent(PlayerEvent event) {
 		switch (event.getType()) {
 		case PLAYER_CREATE: {
+			addCumulativeCount(ConditionTypeEnum.CumulativeLogins, 1);
+			break;
+		}
+		case NewDay: {
+			reset(1);
+			
+			addCumulativeCount(ConditionTypeEnum.CumulativeLogins, 1);
+			break;
+		}
+		case NewWeek: {
+			reset(2);
+			break;
+		}
+		case NewMonth: {
+			reset(3);
+			break;
+		}
+		case WatchAds: {
+			addCumulativeCount(ConditionTypeEnum.WatchAdsCumulation, 1);
+			break;
+		}
+		case Charge: {
+			addCumulativeCount(ConditionTypeEnum.AccumulatedRecharge, event.getIntParameter(0));
+			addCumulativeCount(ConditionTypeEnum.RechargeCnt, 1);
+			break;
+		}
+		case BattleEnd: {
+			addCumulativeCount(ConditionTypeEnum.KillMonsters, event.getIntParameter(3));
+			addCumulativeCount(ConditionTypeEnum.KillBoss, event.getIntParameter(4));
+			break;
+		}
+		case HeroBreak: {
+			int quality = event.getIntParameter(1);
+			addCumulativeCount(ConditionTypeEnum.BreakHeroCumulation, 1);
+			addCumulativeCount(ConditionTypeEnum.EarnHeroCumulation, 1, quality);
+			break;
+		}
+		case Hero: {
+			int id = event.getIntParameter(0);
+			HeroConfig heroConfig = HeroManager.instance().get(id);
+			addCumulativeCount(ConditionTypeEnum.EarnHeroCumulation, 1, heroConfig.InitialQuality);
+			break;
+		}
+		case CostItem: {
+			int id = event.getIntParameter(0);
+			int count = event.getIntParameter(1);
+			if (id == Asset.diamond.ID) {
+				addCumulativeCount(ConditionTypeEnum.ConsumesDiamonds, count);
+			}
+			break;
+		}
+		case Patrol: {
+			boolean isFast = event.getBoolParameter(0);
+			if (isFast) {
+				addCumulativeCount(ConditionTypeEnum.QuickHangUpCumulation, 1);
+			}
+			break;
+		}
+		case Draw: {
+			int count = event.getIntParameter(0);
+			int typeId = event.getIntParameter(1);
+			if (typeId == 2) {
+				addCumulativeCount(ConditionTypeEnum.SupremeGacha, count);
+			}
 			break;
 		}
 		default:
-			throw new IllegalArgumentException("Unexpected value: " + event);
+			// 其他事件不处理
+			break;
+		}
+	}
+
+	private void reset(int type) {
+		IntMapWrapper intMapWrapper = cumulativeCountMap.get(type); 
+		if (intMapWrapper != null) {
+			intMapWrapper.clear();
+		}
+		StringMapWrapper stringMapWrapper = cumulativeCountExtMap.get(type); 
+		if (stringMapWrapper != null) {
+			stringMapWrapper.clear();
 		}
 	}
 
@@ -45,14 +145,18 @@ public class CountingModule extends BasePlayerModule {
 	}
 	
 	public void addCumulativeCount(ConditionTypeEnum type, int count) {
-		cumulativeCountMap.add(type.ID, count);
+		cumulativeCountMap.forEach((k, v) -> {
+			v.add(type.ID, count);
+		});
 	}
 
 	public void addCumulativeCount(ConditionTypeEnum type, int count, int... ext) {
-		if (ext ==null ||  ext.length == 0) {
+		if (ext == null || ext.length == 0) {
 			addCumulativeCount(type, count);
 		} else {
-			cumulativeCountExtMap.add(type.ID, count, ext);
+			cumulativeCountExtMap.forEach((k, v) -> {
+				v.add(type.ID, count, ext);
+			});
 		}
 	}
 	
@@ -62,8 +166,10 @@ public class CountingModule extends BasePlayerModule {
 	 * @param type
 	 * @return
 	 */
-	public int getCumulativeCount(ConditionTypeEnum type) {
-		return this.cumulativeCountMap.getValue(type.ID);
+	public int getCumulativeCount(int condition) {
+		ConditionConfig conditionConfig = ConditionManager.instance().get(condition);
+		IntMapWrapper map = cumulativeCountMap.computeIfAbsent(conditionConfig.resetType, r -> new IntMapWrapper());
+		return map.getValue(conditionConfig.type);
 	}
 
 	/** 
@@ -72,10 +178,18 @@ public class CountingModule extends BasePlayerModule {
 	 * @param ext 额外参数
 	 * @return
 	 */
-	public int getCumulativeCount(ConditionTypeEnum type, int... ext) {
-		if (ext!= null &&  ext.length > 0) {
-			return this.cumulativeCountExtMap.getValue(type.ID, ext);
+	public int getCumulativeCount(int condition, int... ext) {
+		
+		if (ext == null ||  ext.length == 0) {
+			return getCumulativeCount(condition);
 		}
-		return getCumulativeCount(type);
+		ConditionConfig conditionConfig = ConditionManager.instance().get(condition); 
+		StringMapWrapper map = cumulativeCountExtMap.computeIfAbsent(conditionConfig.resetType,r -> new StringMapWrapper()); 
+		return map.getValue(conditionConfig.type, ext);
+	}
+	
+	@Override
+	public int processOrder() {
+		return 100;
 	}
 }
