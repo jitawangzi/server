2bd5ec1f1e16b1414c03a8764c0ca5c20a81ee9d
shuyongqiang <shuyongqiang@cyou-inc.com>
2025-08-26 16:09:07 +0800

    商店刷新

--- game/src/main/java/cn/game/games/net/game/module/shop/ShopModule.java
+++ game/src/main/java/cn/game/games/net/game/module/shop/ShopModule.java
@@ -150,6 +150,7 @@ public class ShopModule extends BasePlayerModule {
 		case 3:
 		case 4: {
 			// 刷新金币、钻石商店
+			shopItemsMap.removeAll(shopId);
 			Collection<RechargeStoreConfig> rechargeStore = RechargeStoreManager.instance().list();
 			for (RechargeStoreConfig rechargeStoreConfig : rechargeStore) {
 				shopItemsMap.put(rechargeStoreConfig.Type, new ShopItem(rechargeStoreConfig.Item));
