88058a44942590ddca2a67fcd3f0b4a4e06e0388
shuyongqiang <shuyongqiang@cyou-inc.com>
2025-09-09 14:25:34 +0800

    1

--- game/src/main/java/cn/game/games/net/game/module/draw/HeroRecruit.java
+++ game/src/main/java/cn/game/games/net/game/module/draw/HeroRecruit.java
@@ -111,7 +111,7 @@ public class HeroRecruit {
 		resp.addAllItemCount(itemCountList);
 		resp.addAllRecruitedPos(recruitedPosList);
 
-		resp.setCanMultiple(recruitCount >= GlobalConst.GachaMultipleCount);
+		resp.setCanMultiple(recruitCount >= GlobalConst.GachaTripleTime);
 		if (notRefresh()) {
 			resp.setFreeRefreshRemaningSeconds(-1);
 		}else {
