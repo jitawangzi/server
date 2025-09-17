9be744dc5a2b053ed8e5e412da71d1ad38b2f37e
shuyongqiang <shuyongqiang@cyou-inc.com>
2025-09-17 10:08:45 +0800

    工会等级条件

--- game/src/main/java/cn/game/games/net/game/module/quest/require/GuildLevel.java
+++ game/src/main/java/cn/game/games/net/game/module/quest/require/GuildLevel.java
@@ -2,13 +2,13 @@ package cn.game.games.net.game.module.quest.require;
 
 import cn.game.games.core.event.EventTypeEnum;
 import cn.game.games.core.event.PlayerEvent;
-import cn.game.games.net.game.module.quest.AbstractCondition;
+import cn.game.games.net.game.module.quest.AbstractCumulativeCondition;
 import cn.game.games.net.game.module.quest.ConditionType;
 import cn.game.protocol.generated.enume.ConditionTypeEnum;
 
 @ConditionType(type = ConditionTypeEnum.GuildLevel)
-public class GuildLevel extends AbstractCondition {
-	private static final EventTypeEnum[] events = new EventTypeEnum[] {  };
+public class GuildLevel extends AbstractCumulativeCondition {
+	private static final EventTypeEnum[] events = new EventTypeEnum[] {EventTypeEnum.GuildJoin };
 
 	@Override
 	public EventTypeEnum[] getEventTypes() {
@@ -21,6 +21,6 @@ public class GuildLevel extends AbstractCondition {
 
 	@Override
 	public boolean checkEventParam(PlayerEvent event) {
-		return false ; 
+		return true ; 
 	}
 }
