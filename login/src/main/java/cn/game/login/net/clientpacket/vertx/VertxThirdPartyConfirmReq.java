e82b5efd088ba7dd1d7abaedf6370bdcf0d113d1
shuyongqiang <shuyongqiang@cyou-inc.com>
2025-10-11 13:52:16 +0800

    登陆返回畅游sdk  user_id

--- login/src/main/java/cn/game/login/net/clientpacket/vertx/VertxThirdPartyConfirmReq.java
+++ login/src/main/java/cn/game/login/net/clientpacket/vertx/VertxThirdPartyConfirmReq.java
@@ -277,7 +277,8 @@ public class VertxThirdPartyConfirmReq implements BaseVertxHandler {
 	private void sendSuccessResponse(HttpServerResponse response, User user) {
 		AccountLoginResponse responseProto = AccountLoginResponse.newBuilder()
 				.setPassportSessionId(String.valueOf(user.getSessionId()))
-				.setUserId(String.valueOf(user.getUsername()))
+				.setUserId(String.valueOf(user.getId()))
+				.setSdkUserId(user.getUsername())
 				.setExt(user.getExtInfo() == null ? "" : user.getExtInfo())
 				.build();
 		response.end(Buffer.buffer(responseProto.toByteArray()));
