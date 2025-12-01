3693e76b4f4704705217ee01ab43c7933326c83d
gh_quyukun <email@sample.com>
2025-12-01 16:29:50 +0800

    【测试自用任务】【大圣擂台】大声擂台赛季结束日榜清空出错 PA202512015860

--- game/src/main/java/cn/game/games/net/game/module/rank/RankService.java
+++ game/src/main/java/cn/game/games/net/game/module/rank/RankService.java
@@ -859,8 +859,8 @@ public class RankService {
 						String content = JsonUtil.toJsonStringWithType(playerRank);
 						MailHelper.addGlobalGmMail(content, serverId, start, end, (byte) MailType.DASHENG_XUN_SHAN.getValue());
 						log.info("removeRank1 serverId:{}", serverId);
-						CompletableFuture<Void> removeRankFuture = removeRankAsync(RankType.DaShengLeiTaiSeason)
-								.thenCompose(v -> {return removeRankAsync(RankType.DaShengLeiTaiDay);}).thenCompose(
+						CompletableFuture<Void> removeRankFuture = removeRankAsync(RankType.DaShengLeiTaiSeason,serverId)
+								.thenCompose(v -> {return removeRankAsync(RankType.DaShengLeiTaiDay,serverId);}).thenCompose(
 										v -> {return setNpcToRankTest(serverId);}
 								);
 						return removeRankFuture;
