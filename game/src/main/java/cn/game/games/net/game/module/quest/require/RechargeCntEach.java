4219b757b7cf75baab08a8e03bb1ce07cff60dfc
shuyongqiang <shuyongqiang@cyou-inc.com>
2025-12-01 11:36:35 +0800

    新条件类型

---
+++ game/src/main/java/cn/game/games/net/game/module/quest/require/RechargeCntEach.java
@@ -0,0 +1,28 @@
+package cn.game.games.net.game.module.quest.require;
+
+import cn.game.games.core.event.EventTypeEnum;
+import cn.game.games.core.event.PlayerEvent;
+import cn.game.games.net.game.module.quest.AbstractCondition;
+import cn.game.games.net.game.module.quest.ConditionType;
+import cn.game.protocol.generated.enume.ConditionTypeEnum;
+
+@ConditionType(type = ConditionTypeEnum.RechargeCntEach)
+public class RechargeCntEach extends AbstractCondition {
+	private static final EventTypeEnum[] events = new EventTypeEnum[] { EventTypeEnum.Charge };
+	@Override
+	public EventTypeEnum[] getEventTypes() {
+		return events;
+	}
+	public RechargeCntEach() {
+
+	}
+
+	@Override
+	public boolean checkEventParam(PlayerEvent event) {
+//		int id = event.getIntParameter(0);
+//		EquipConfig equipConfig = EquipManager.instance().get(id); 
+//		return equipConfig.quality >= getParam();
+		
+		return false;
+	}
+}
