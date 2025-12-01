1b735ecfa90dbea8c9669f9a0cccc3e60c10f4f8
gh_quyukun <email@sample.com>
2025-12-01 16:03:15 +0800

    【非1日版本】【服务器】舍利抽奖、洗炼 PA202511273271-4

---
+++ simulationclient/src/main/java/cn/game/simulation/test/gen/GuildGVEBuyTicketRequest_40000090Test.java
@@ -0,0 +1,35 @@
+package cn.game.simulation.test.gen;
+
+import com.google.protobuf.Message;
+import org.springframework.stereotype.Component;
+
+import cn.game.simulation.client.Client;
+import cn.game.simulation.test.base.ServerTest;
+
+@Component
+public class GuildGVEBuyTicketRequest_40000090Test extends ServerTest{
+
+	@Override
+	public Message getMessage(Client client) {
+		cn.game.protocol.protobuf.GuildMsg.GuildGVEBuyTicketRequest_40000090.Builder builder = cn.game.protocol.protobuf.GuildMsg.GuildGVEBuyTicketRequest_40000090.newBuilder() ; 
+		
+		
+		
+		return builder.build() ; 
+	}
+	
+	@Override
+	public Message getMessagePressure(Client client) {
+		cn.game.protocol.protobuf.GuildMsg.GuildGVEBuyTicketRequest_40000090.Builder builder = cn.game.protocol.protobuf.GuildMsg.GuildGVEBuyTicketRequest_40000090.newBuilder() ; 
+		
+		
+		
+		return builder.build() ; 
+	}
+	
+	public static void main(String args[]) throws Exception {
+	    GuildGVEBuyTicketRequest_40000090Test instance = new GuildGVEBuyTicketRequest_40000090Test();
+	    instance.start();
+	}
+
+}
---
+++ simulationclient/src/main/java/cn/game/simulation/test/gen/GuildGVEOpenMapRequest_40000087Test.java
@@ -0,0 +1,35 @@
+package cn.game.simulation.test.gen;
+
+import com.google.protobuf.Message;
+import org.springframework.stereotype.Component;
+
+import cn.game.simulation.client.Client;
+import cn.game.simulation.test.base.ServerTest;
+
+@Component
+public class GuildGVEOpenMapRequest_40000087Test extends ServerTest{
+
+	@Override
+	public Message getMessage(Client client) {
+		cn.game.protocol.protobuf.GuildMsg.GuildGVEOpenMapRequest_40000087.Builder builder = cn.game.protocol.protobuf.GuildMsg.GuildGVEOpenMapRequest_40000087.newBuilder() ; 
+		
+		
+		
+		return builder.build() ; 
+	}
+	
+	@Override
+	public Message getMessagePressure(Client client) {
+		cn.game.protocol.protobuf.GuildMsg.GuildGVEOpenMapRequest_40000087.Builder builder = cn.game.protocol.protobuf.GuildMsg.GuildGVEOpenMapRequest_40000087.newBuilder() ; 
+		
+		
+		
+		return builder.build() ; 
+	}
+	
+	public static void main(String args[]) throws Exception {
+	    GuildGVEOpenMapRequest_40000087Test instance = new GuildGVEOpenMapRequest_40000087Test();
+	    instance.start();
+	}
+
+}
