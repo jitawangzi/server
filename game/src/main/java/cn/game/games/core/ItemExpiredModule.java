4d27aaa7f9099c6238378a6b840852e3bc6d8773
shuyongqiang <shuyongqiang@cyou-inc.com>
2025-12-02 15:50:57 +0800

    一点道具过期代码，未完成

--- game/src/main/java/cn/game/games/core/ItemExpiredModule.java
+++ game/src/main/java/cn/game/games/core/ItemExpiredModule.java
@@ -29,9 +29,10 @@ public class ItemExpiredModule extends BasePlayerModule {
 					if (item.getExpiredTime() <= now) {
 						toRemove.add(item);
 					} else {
-						player.setTimerTask((item.getExpiredTime() - now) * 1000, r -> {
+						long expiredTimeTask =  player.setTimerTask((item.getExpiredTime() - now) * 1000, r -> {
 							goodsModule.delItem(item, OpType.Expired);
 						});
+						item.setExpiredTimeTask(expiredTimeTask);
 					}
 				}
 			}
