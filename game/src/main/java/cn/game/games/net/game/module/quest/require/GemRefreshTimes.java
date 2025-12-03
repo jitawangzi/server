6458541f0b9d148500f582216dd0f9c18349a750
shuyongqiang <shuyongqiang@cyou-inc.com>
2025-12-03 15:51:36 +0800

    新增一些条件类型，逻辑未实现

---
+++ game/src/main/java/cn/game/games/net/game/module/quest/require/GemRefreshTimes.java
@@ -0,0 +1,26 @@
+package cn.game.games.net.game.module.quest.require;
+
+import cn.game.games.core.event.EventTypeEnum;
+import cn.game.games.core.event.PlayerEvent;
+import cn.game.games.net.game.module.quest.AbstractCondition;
+import cn.game.games.net.game.module.quest.ConditionType;
+import cn.game.protocol.generated.enume.ConditionTypeEnum;
+
+@ConditionType(type = ConditionTypeEnum.GemRefreshTimes)
+public class GemRefreshTimes extends AbstractCondition {
+	private static final EventTypeEnum[] events = new EventTypeEnum[] { EventTypeEnum.EquipPartStrength };
+
+	@Override
+	public EventTypeEnum[] getEventTypes() {
+		return events;
+	}
+
+	public GemRefreshTimes() {
+
+	}
+
+	@Override
+	public boolean checkEventParam(PlayerEvent event) {
+		return true; 
+	}
+}
